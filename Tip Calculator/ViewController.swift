06357e83b32b0af271989cfc57865fc477110990
Kwaku Oteng <kwakuoteng@kwakus-macbook-air.local>
2019-01-24 17:43:05 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Kwaku Oteng on 1/24/19.
+//  Copyright © 2019 Kwaku Oteng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
